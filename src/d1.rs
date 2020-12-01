5723e132fcdb7394a6411225205bdda44b358746
Lakret <Rakuretu@gmail.com>
2020-12-01 21:38:15 +0100

    day 1

---
+++ src/d1.rs
@@ -0,0 +1,84 @@
+use std::collections::HashSet;
+
+pub fn solve(input: &str) -> Option<i64> {
+  let numbers = parse(input);
+
+  if let Some((x, y)) = find_complement(&numbers, 0) {
+    return Some(x * y);
+  }
+
+  None
+}
+
+pub fn solve2(input: &str) -> Option<i64> {
+  let numbers = parse(input);
+
+  // do a brute-force pass to find 3 numbers instead of 2,
+  // using optimized solution for 2 numbers internally.
+  for z in numbers.iter() {
+    if let Some((x, y)) = find_complement(&numbers, *z) {
+      return Some(x * y * z);
+    }
+  }
+
+  None
+}
+
+// Helpers
+
+fn parse(input: &str) -> HashSet<i64> {
+  input
+    .trim_end()
+    .split("\n")
+    .map(|number| number.parse::<i64>().unwrap())
+    .collect::<HashSet<_>>()
+}
+
+// Returns a pair of numbers `x` and `y`, that together with `z`
+// sum to `2020`.
+// If nothing is found, returns `None`.
+fn find_complement(numbers: &HashSet<i64>, z: i64) -> Option<(i64, i64)> {
+  for x in numbers.iter() {
+    let y = 2020 - x - z;
+    if numbers.contains(&y) {
+      return Some((*x, y));
+    }
+  }
+
+  None
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+  use std::fs;
+
+  const SAMPLE_INPUT: &'static str = "1721
+979
+366
+299
+675
+1456";
+
+  #[test]
+  fn part_one_works_with_sample() {
+    assert_eq!(solve(SAMPLE_INPUT), Some(514579));
+  }
+
+  #[test]
+  fn part_two_works_with_sample() {
+    assert_eq!(solve2(SAMPLE_INPUT), Some(241861950));
+  }
+
+  #[test]
+  fn part_one_works() {
+    let input = fs::read_to_string("inputs/d1").unwrap();
+    assert_eq!(solve(&input), Some(921504));
+  }
+
+  #[test]
+  fn part_two_works() {
+    let input = fs::read_to_string("inputs/d1").unwrap();
+    assert_eq!(solve2(&input), Some(195700142));
+  }
+}
---
+++ src/main.rs
@@ -0,0 +1,76 @@
+use std::collections::HashMap;
+use std::fs;
+use std::path::Path;
+
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
+mod d1;
+
+type Solver = fn(&str) -> Option<i64>;
+const COMMANDS: [(&'static str, Solver); 2] =
+  [("d1", d1::solve), ("d1_2", d1::solve2)];
+
+fn parse_line(line: &str) -> Option<(&str, &str)> {
+  let items = line.split_ascii_whitespace().collect::<Vec<_>>();
+  match &items[..] {
+    // Default case for each day is to use its input file
+    &[day] => day
+      // ignore the task part to get the filename
+      .split("_")
+      .take(1)
+      .next()
+      .map(|input_file| (day, input_file)),
+    &[day, input_file] => Some((day, input_file)),
+    _ => None,
+  }
+}
+
+fn main() {
+  let mut rl = Editor::<()>::new();
+  let commands: HashMap<&'static str, Solver> =
+    COMMANDS.iter().cloned().collect();
+
+  loop {
+    let readline = rl.readline(">>> ");
+
+    match readline {
+      Ok(line) => {
+        rl.add_history_entry(line.as_str());
+
+        if let Some((solver, input_file)) = parse_line(&line) {
+          let input_file = Path::new("inputs").join(input_file);
+
+          match commands.get(solver) {
+            None => println!("Unrecoginzed command: {:?}.", &line),
+            Some(solver) => match fs::read_to_string(&input_file) {
+              Ok(input) => {
+                let result = solver(&input);
+                println!("{:?}", result);
+              }
+              Err(error) => println!(
+                "Cannot read input file {:?} due to {:?}.",
+                &input_file, error
+              ),
+            },
+          }
+        }
+      }
+
+      Err(ReadlineError::Interrupted) => {
+        println!("CTRL-C");
+        break;
+      }
+
+      Err(ReadlineError::Eof) => {
+        println!("CTRL-D");
+        break;
+      }
+
+      Err(err) => {
+        println!("Error: {:?}", err);
+        break;
+      }
+    }
+  }
+}
