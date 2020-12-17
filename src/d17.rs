4e53e4417c2f28777592747bf1807fd24f41eac7
Lakret <rakuretu@gmail.com>
2020-12-18 00:35:03 +0100

    d17 p2

--- src/d17.rs
+++ src/d17.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 use std::ops::RangeInclusive;
 
 pub fn solve(input: &str) -> Option<Box<usize>> {
-  let mut cube = Cube::parse(input);
+  let mut cube = Cube::parse(input, 3);
 
   for _turn in 0..6 {
     cube = cube.advance();
@@ -12,71 +12,69 @@ pub fn solve(input: &str) -> Option<Box<usize>> {
 }
 
 pub fn solve2(input: &str) -> Option<Box<usize>> {
-  None
+  let mut cube = Cube::parse(input, 4);
+
+  for _turn in 0..6 {
+    cube = cube.advance();
+  }
+
+  Some(Box::new(cube.active.len()))
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct Point {
-  x: i64,
-  y: i64,
-  z: i64,
+  coords: Vec<i64>,
 }
 
 impl Point {
-  fn neighbours(&self) -> Vec<Point> {
-    let mut points = Vec::with_capacity(26);
+  fn neighbours(&self) -> HashSet<Point> {
+    let mut points = HashSet::new();
 
-    for delta_x in [-1, 0, 1].iter() {
-      for delta_y in [-1, 0, 1].iter() {
-        for delta_z in [-1, 0, 1].iter() {
-          if !(*delta_x == 0 && *delta_y == 0 && *delta_z == 0) {
-            let point = Point {
-              x: self.x + delta_x,
-              y: self.y + delta_y,
-              z: self.z + delta_z,
-            };
-            points.push(point);
-          }
+    points.insert(self.clone());
+    for dimension_id in 0..self.coords.len() {
+      for point in points.iter().cloned().collect::<Vec<_>>() {
+        for delta in [-1, 0, 1].iter() {
+          let mut point = point.clone();
+          point.coords[dimension_id] = point.coords[dimension_id] + delta;
+
+          points.insert(point);
         }
       }
     }
 
+    points.remove(self);
     points
   }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct Cube {
-  xs: RangeInclusive<i64>,
-  ys: RangeInclusive<i64>,
-  zs: RangeInclusive<i64>,
+  // matches dimension_id with active coordinate range
+  border: Vec<RangeInclusive<i64>>,
   active: HashSet<Point>,
-}
-
-impl std::default::Default for Cube {
-  fn default() -> Cube {
-    Cube {
-      xs: 0..=0,
-      ys: 0..=0,
-      zs: 0..=0,
-      active: HashSet::new(),
-    }
-  }
+  dimensions: usize,
 }
 
 impl Cube {
-  fn parse(input: &str) -> Cube {
-    let mut cube = Cube::default();
+  fn empty(dimensions: usize) -> Cube {
+    Cube {
+      border: vec![0..=0; dimensions],
+      active: HashSet::new(),
+      dimensions,
+    }
+  }
+
+  fn parse(input: &str, dimensions: usize) -> Cube {
+    let mut cube = Cube::empty(dimensions);
 
     for (y, row) in input.trim_end().split('\n').enumerate() {
       for (x, ch) in row.chars().enumerate() {
         if ch == '#' {
-          let point = Point {
-            x: x as i64,
-            y: y as i64,
-            z: 0,
-          };
-          cube.activate(point);
+          let mut coords = vec![0; dimensions];
+          coords[0] = x as i64;
+          coords[1] = y as i64;
+
+          cube.activate(Point { coords });
         }
       }
     }
@@ -85,27 +83,32 @@ impl Cube {
   }
 
   fn activate(&mut self, point: Point) {
-    self.xs = extend(&self.xs, point.x);
-    self.ys = extend(&self.ys, point.y);
-    self.zs = extend(&self.zs, point.z);
+    for (dimension_id, &coord) in point.coords.iter().enumerate() {
+      self.border[dimension_id] = extend(&self.border[dimension_id], coord);
+    }
 
     self.active.insert(point);
   }
 
-  fn is_bodering_or_inside(&self, point: &Point) -> bool {
-    (self.xs.contains(&point.x) || (self.xs.start() - point.x).abs() <= 1 || (self.xs.end() - point.x).abs() <= 1)
-      && (self.ys.contains(&point.y) || (self.ys.start() - point.y).abs() <= 1 || (self.ys.end() - point.y).abs() <= 1)
-      && (self.zs.contains(&point.z) || (self.zs.start() - point.z).abs() <= 1 || (self.zs.end() - point.z).abs() <= 1)
+  fn is_bordering_or_inside(&self, point: &Point) -> bool {
+    (0..self.dimensions).into_iter().all(|dimension_id| {
+      let dimension_border = &self.border[dimension_id];
+      let dimension_coord = point.coords[dimension_id];
+
+      dimension_border.contains(&dimension_coord)
+        || (dimension_border.start() - dimension_coord).abs() <= 1
+        || (dimension_border.end() - dimension_coord).abs() <= 1
+    })
   }
 
   fn advance(self) -> Cube {
-    let mut next = Cube::default();
+    let mut next = Cube::empty(self.dimensions);
     let mut consider = self.active.iter().cloned().collect::<Vec<_>>();
     let mut seen = HashSet::new();
 
     while let Some(point) = consider.pop() {
       if !seen.contains(&point)
-        && (self.active.contains(&point) || self.is_bodering_or_inside(&point) || next.is_bodering_or_inside(&point))
+        && (self.active.contains(&point) || self.is_bordering_or_inside(&point) || next.is_bordering_or_inside(&point))
       {
         let neighbours = point.neighbours();
         let active_neighbours = neighbours
@@ -115,19 +118,19 @@ impl Cube {
 
         if self.active.contains(&point) {
           if active_neighbours == 2 || active_neighbours == 3 {
-            next.activate(point);
+            next.activate(point.clone());
           }
         } else {
           if active_neighbours == 3 {
-            next.activate(point);
+            next.activate(point.clone());
           }
         }
 
         seen.insert(point);
 
-        for &neighbour in neighbours.iter() {
-          if !seen.contains(&neighbour) {
-            consider.push(neighbour);
+        for neighbour in neighbours.iter() {
+          if !seen.contains(neighbour) {
+            consider.push(neighbour.clone());
           }
         }
       }
@@ -159,16 +162,20 @@ mod tests {
   #[test]
   fn part_one_solved() {
     let input = fs::read_to_string("inputs/sample17").unwrap();
-
-    let cube = Cube::parse(&input);
+    let cube = Cube::parse(&input, 3);
     assert_eq!(cube.active.len(), input.chars().filter(|ch| *ch == '#').count());
-
     assert_eq!(solve(&input), Some(Box::new(112)));
+
+    let input = fs::read_to_string("inputs/d17").unwrap();
+    assert_eq!(solve(&input), Some(Box::new(313)));
   }
 
   #[test]
   fn part_two_solved() {
+    let input = fs::read_to_string("inputs/sample17").unwrap();
+    assert_eq!(solve2(&input), Some(Box::new(848)));
+
     let input = fs::read_to_string("inputs/d17").unwrap();
-    assert_eq!(solve2(&input), None);
+    assert_eq!(solve2(&input), Some(Box::new(2640)));
   }
 }
