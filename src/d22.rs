9855d9fa48442cd0e0069b18b29d5f1787315cee
Lakret <Rakuretu@gmail.com>
2020-12-22 19:50:20 +0100

    d22 part1

--- src/d22.rs
+++ src/d22.rs
@@ -1,16 +1,37 @@
-pub fn solve(input: &str) -> Option<Box<usize>> {
-  let (deck1, deck2) = parse(input);
-  dbg!(&deck1);
-  dbg!(&deck2);
+use std::collections::VecDeque;
 
+pub fn solve(input: &str) -> Option<Box<u64>> {
+  let (mut deck1, mut deck2) = parse(input);
+
+  while !deck1.is_empty() && !deck2.is_empty() {
+    let top_card1 = deck1.pop_front().unwrap();
+    let top_card2 = deck2.pop_front().unwrap();
+
+    if top_card1 > top_card2 {
+      deck1.push_back(top_card1);
+      deck1.push_back(top_card2);
+    } else {
+      deck2.push_back(top_card2);
+      deck2.push_back(top_card1);
+    }
+  }
+
+  let winning_deck = if deck1.is_empty() { deck2 } else { deck1 };
+
+  let answer = winning_deck
+    .iter()
+    .rev()
+    .enumerate()
+    .fold(0, |acc, (idx, elem)| acc + ((idx + 1) as u64 * elem));
+
+  Some(Box::new(answer))
+}
+
+pub fn solve2(input: &str) -> Option<Box<u64>> {
   None
 }
 
-pub fn solve2(input: &str) -> Option<Box<usize>> {
-  None
-}
-
-fn parse(input: &str) -> (Vec<u64>, Vec<u64>) {
+fn parse(input: &str) -> (VecDeque<u64>, VecDeque<u64>) {
   let mut decks = Vec::with_capacity(2);
 
   for deck in input.trim_end().split("\n\n") {
@@ -18,7 +39,7 @@ fn parse(input: &str) -> (Vec<u64>, Vec<u64>) {
       .split('\n')
       .skip(1)
       .map(|card| card.parse::<u64>().unwrap())
-      .collect::<Vec<_>>();
+      .collect::<VecDeque<_>>();
     decks.push(cards);
   }
 
@@ -32,8 +53,11 @@ mod tests {
 
   #[test]
   fn part_one_solved() {
+    let input = fs::read_to_string("inputs/sample22").unwrap();
+    assert_eq!(solve(&input), Some(Box::new(306)));
+
     let input = fs::read_to_string("inputs/d22").unwrap();
-    assert_eq!(solve(&input), None);
+    assert_eq!(solve(&input), Some(Box::new(32598)));
   }
 
   #[test]
